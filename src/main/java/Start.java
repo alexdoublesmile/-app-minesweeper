480b8bbcf3721543051c17ed75541e69ed260664
Александр Плохой <plohoy@zen-it.ru>
2023-01-20 09:23:22 +0300

    add dimensions

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -4,6 +4,9 @@ import java.awt.*;
 public class Start extends JFrame {
 
     private JPanel panel;
+    private final int COLS = 15;
+    private final int ROWS = 1;
+    private final int IMAGE_SIZE = 50;
 
     public static void main(String[] args) {
         new Start();
@@ -25,7 +28,7 @@ public class Start extends JFrame {
 
     private void initPanel() {
         panel = new JPanel();
-        panel.setPreferredSize(new Dimension(500, 500));
+        panel.setPreferredSize(new Dimension(COLS*IMAGE_SIZE, ROWS*IMAGE_SIZE));
         add(panel);
     }
 }
