dc3ba43a369e73180a35f0bc303ab452d510398f
Александр Плохой <plohoy@zen-it.ru>
2023-01-20 10:04:16 +0300

    add coordinates model

---
+++ src/main/java/model/Coord.java
@@ -0,0 +1,46 @@
+package model;
+
+import java.util.Objects;
+
+public class Coord {
+    private int x;
+    private int y;
+
+    public Coord(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Coord() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coord coord = (Coord) o;
+        return x == coord.x &&
+                y == coord.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
