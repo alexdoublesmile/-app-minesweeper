6c76404c30cb204b0a7c93a1db6236ae72aec682
Александр Плохой <alexplohoy@mail.ru>
2023-01-25 07:44:02 +0300

    feat(config): add config

--- src/main/java/view/swing/menu/AboutAction.java
+++ src/main/java/view/swing/menu/AboutAction.java
@@ -4,9 +4,16 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import static view.util.ViewConstants.VERSION_MESSAGE_TEXT;
+import static view.util.ViewConstants.VERSION_MESSAGE_TITLE;
+
 public class AboutAction implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
-        JOptionPane.showMessageDialog(null, "message");
+        JOptionPane.showMessageDialog(
+                null,
+                VERSION_MESSAGE_TEXT,
+                VERSION_MESSAGE_TITLE,
+                JOptionPane.INFORMATION_MESSAGE)
     }
 }
--- src/main/java/view/util/ViewConstants.java
+++ src/main/java/view/util/ViewConstants.java
@@ -1,9 +1,21 @@
 package view.util;
 
+import config.PropertyConfig;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class ViewConstants {
-    public final String VIEW_TITLE = "Minesweeper";
-    public final String GREETING_LABEL = "Welcome!";
+    public static final String VIEW_TITLE = "Minesweeper";
+    public static final String GREETING_LABEL = "Welcome!";
+    public static final String VERSION_MESSAGE_TITLE = "Graphics Tablet version";
+
+    public static final String VERSION = PropertyConfig.getProperty("version.number");
+    public static final String VERSION_DATE = PropertyConfig.getProperty("version.date");
+    public static final String PROGRAM_CREATOR = PropertyConfig.getProperty("creator");
+    public static final String VERSION_MESSAGE_TEXT =
+            String.format("<html>" +
+                            "<p>Minesweeper v.%s</p>" +
+                            "<p>revision %s</p>" +
+                            "<p style=\"color: red; text-align:center\">by %s</p>",
+                    VERSION, VERSION_DATE, PROGRAM_CREATOR);
 }
